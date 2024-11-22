a01b9ab3e42a8e48b89e514836c5d7d497c51a5f
baxthus <dev@baxt.fun>
2024-11-22 14:11:51 -0300

    feat: cool title effect

--- src/app.vue
+++ src/app.vue
@@ -1,3 +1,21 @@
+<script setup lang="ts">
+const fullTitle = 'baxthus';
+let title = '';
+let titleIndex = 1;
+
+onMounted(() => {
+    setInterval(() => {
+        if (titleIndex > fullTitle.length) {
+            titleIndex = 1;
+        }
+
+        title = fullTitle.slice(0, titleIndex);
+        useHead({ title: title });
+        titleIndex++;
+    }, 500);
+});
+</script>
+
 <template>
     <div class="min-h-screen flex flex-col items-center justify-center">
         <NuxtPage />
