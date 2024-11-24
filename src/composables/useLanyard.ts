9859ccab793137210dd8512d6aef9cba8a413915
baxthus <dev@baxt.fun>
2024-11-24 13:49:19 -0300

    fix: better socket disconnection  handling

--- src/composables/useLanyard.ts
+++ src/composables/useLanyard.ts
@@ -5,58 +5,7 @@ export const WEBSOCKET_URL = 'wss://api.lanyard.rest/socket';
 
 export async function useLanyard(options: LanyardOptions): Promise<LanyardResponse | Array<LanyardResponse> | undefined> {
     if (Reflect.has(options, 'socket')) {
-        const { onPresenceUpdate } = options;
-
-        // Mozilla wants to be special
-        const supportsSockets = 'WebSocket' in window || 'MozWebSocket' in window;
-
-        if (!supportsSockets) throw new Error('WebSockets are not supported in this environment');
-
-        const subscription = typeof options.userId === 'object' ? 'subscribe_to_ids' : 'subscribe_to_id';
-
-        let socket = new WebSocket(WEBSOCKET_URL);
-        let heartbeatInterval = 30 * 1000;
-
-        socket.addEventListener('open', () => {
-            socket.send(JSON.stringify({
-                op: 2,
-                d: {
-                    [subscription]: options.userId,
-                },
-            }));
-
-            setInterval(() => {
-                socket.send(JSON.stringify({ op: 3 }));
-            }, heartbeatInterval);
-        });
-
-        socket.addEventListener('message', (event) => {
-            const data = JSON.parse(event.data) as {
-                op: 0;
-                d: LanyardData | Record<string, LanyardData>;
-            } | {
-                op: 1;
-                d: {
-                    heartbeat_interval: number;
-                };
-            };
-
-            if (data.op === 0) {
-                onPresenceUpdate?.(data.d);
-            }
-            if (data.op === 1) {
-                heartbeatInterval = data.d.heartbeat_interval;
-            }
-        });
-
-        // Heartbeat may not be sent if the page is unfocused due to the browser putting the tab to sleep
-        // There is no way to prevent this, so we just reconnect the socket
-        socket.addEventListener('close', () => {
-            // Clean the data so things that need live data don't get buggy (ex: Spotify presence)
-            onPresenceUpdate?.({} as LanyardData);
-            socket = new WebSocket(WEBSOCKET_URL); // Not sure if works
-        });
-
+        handleSocket(options);
         return;
     }
 
@@ -81,4 +30,62 @@ export async function useLanyard(options: LanyardOptions): Promise<LanyardRespon
     }
 
     return responseArray;
+}
+
+function handleSocket(options: LanyardOptions) {
+    const { onPresenceUpdate } = options;
+
+    // Mozilla wants to be special
+    const supportsSockets = 'WebSocket' in window || 'MozWebSocket' in window;
+
+    if (!supportsSockets) throw new Error('WebSockets are not supported in this environment');
+
+    const subscription = typeof options.userId === 'object' ? 'subscribe_to_ids' : 'subscribe_to_id';
+
+    const socket = new WebSocket(WEBSOCKET_URL);
+    let heartbeatInterval = 30 * 1000;
+
+    socket.addEventListener('open', () => {
+        socket.send(JSON.stringify({
+            op: 2,
+            d: {
+                [subscription]: options.userId,
+            },
+        }));
+
+        setInterval(() => {
+            socket.send(JSON.stringify({ op: 3 }));
+        }, heartbeatInterval);
+    });
+
+    socket.addEventListener('message', (event) => {
+        const data = JSON.parse(event.data) as {
+            op: 0;
+            d: LanyardData | Record<string, LanyardData>;
+        } | {
+            op: 1;
+            d: {
+                heartbeat_interval: number;
+            };
+        };
+
+        if (data.op === 0) {
+            onPresenceUpdate?.(data.d);
+        }
+        if (data.op === 1) {
+            heartbeatInterval = data.d.heartbeat_interval;
+        }
+    });
+
+    // Heartbeat may not be sent if the page is unfocused due to the browser putting the tab to sleep
+    // Happens more often on Apple devices, as they are more aggressive with not running background tasks
+    // There is no way to prevent this, so we just reconnect the socket
+    socket.addEventListener('close', () => {
+        // Taken from https://stackoverflow.com/a/23176223
+        console.log('Socket closed, reconnecting in 1 second');
+        setTimeout(() => {
+            // Garbage collector goes brrrr
+            handleSocket(options);
+        }, 1000);
+    });
 }
