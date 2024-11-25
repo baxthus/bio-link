976e8aca5df53a4a01936bc44cc4045e042fba05
baxthus <dev@baxt.fun>
2024-11-25 15:09:12 -0300

    feat: remove title animation

--- src/app.vue
+++ src/app.vue
@@ -1,30 +1,3 @@
-<script setup lang="ts">
-const title = {
-    running: true,
-    full: 'baxthus',
-    current: '',
-    index: 1,
-};
-
-onMounted(() => {
-    setInterval(() => {
-        if (!title.running) {
-            title.index = 1;
-            useHead({ title: title.full });
-            return;
-        }
-        if (title.index > title.full.length) title.index = 1;
-
-        title.current = title.full.slice(0, title.index);
-        useHead({ title: title.current });
-        title.index++;
-    }, 500);
-
-    addEventListener('focus', () => title.running = true);
-    addEventListener('blur', () => title.running = false);
-});
-</script>
-
 <template>
     <div class="sm:h-screen pt-4 sm:pt-0 flex flex-col items-center justify-center">
         <NuxtPage />
